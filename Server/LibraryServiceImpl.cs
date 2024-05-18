4c586473d9782a183069e5b05e5eecf354ce3441
gszp1 <grzybekmateusz@poczta.fm>
2024-05-18 21:11:52 +0200

    Adds Server project.

---
+++ Server/LibraryServiceImpl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryService.DataContracts;
+using LibraryService.ServiceContracts;
+
+namespace Server
+{
+    public class LibraryServiceImpl : ILibraryService
+    {
+        public int[] FindBooks(string keyword)
+        {
+            List<int> booksIdentifiers = new List<int>();
+
+            return booksIdentifiers.ToArray();
+        }
+
+        public BookInfo GetBookInfo(int bookID)
+        {
+            BookInfo bookInfo = new BookInfo();
+            return bookInfo;
+        }
+    }
+}
