059dbd5bcc92994f9555fa9c379af35c04eda413
gszp1 <grzybekmateusz@poczta.fm>
2024-05-19 11:17:24 +0200

    Adds reading client configuration.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Client
+﻿using System;
+using System.Configuration;
+
+namespace Client
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string uriString = string.Format(
+                "net.tcp//{0}:{1}/{2}",
+                ConfigurationManager.AppSettings["ServiceAddress"],
+                ConfigurationManager.AppSettings["ServicePort"],
+                ConfigurationManager.AppSettings["ServiceName"]
+            );
+            Uri uri = new Uri(uriString);
+            Console.WriteLine($"Used uri: { uriString}");
         }
     }
 }
